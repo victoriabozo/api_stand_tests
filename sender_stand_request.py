f649b49c69af717232f8a4b0269a2baa4a47c26f
Victoria Bozo <bozovictoria@gmail.com>
2025-08-29 10:31:59 -0600

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -4,8 +4,8 @@ import data
 
 def post_new_user(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-                         json=body,
-                         headers=data.headers)
+                         json = body,
+                         headers = data.headers)
 
 def get_users_table():
     return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH,
