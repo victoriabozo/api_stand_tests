77834a55b2eb5268d4dc9ee5a50d6205b4d8acde
Victoria Bozo <bozovictoria@gmail.com>
2025-08-29 10:29:55 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,13 @@
-user_body={"firstName": "Andrea", "phone": "+11234567890", "address": "123 Elm Street, Hilltop"}
-headers={"Content-Type": "application/json"}
+user_body = {
+    "firstName": "Andrea", 
+    "phone": "+11234567890", 
+    "address": "123 Elm Street, Hilltop"
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
 product_ids = {
     "ids": [1, 2, 3]
 }
